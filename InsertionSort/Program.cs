eab72b922e387509e2d3c7dbcb9ab1dd57d79db6
Lochie Denholm <lochlan.denholm@informatel.com>
2019-09-10 15:43:03 +1000

    simple

--- InsertionSort/Program.cs
+++ InsertionSort/Program.cs
@@ -6,7 +6,28 @@ namespace InsertionSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 3, 25, 21, 17, 29, 13, 2 };
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int temp = arr[i];
+                int j = i;
+
+                while (j > 0 && temp < arr[j - 1])
+                {
+                    arr[j] = arr[j - 1];
+                    j--;
+                }
+
+                arr[j] = temp;
+            }
+
+            foreach (var i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
         }
     }
 }
